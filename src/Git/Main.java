b05b805f2b976684b9fa481051671c2f0f20682a
Kamal <kamal.madaliev@gmail.com>
2021-04-12 00:55:09 +0600

    Add new class in main branch

--- src/Git/Main.java
+++ src/Git/Main.java
@@ -1,5 +1,9 @@
 package Git;
-
+    import java.util.Scanner;
 public class Main {
-
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int x= in.nextInt();
+        Class1 class1 = new Class1(15);
+    }
 }
